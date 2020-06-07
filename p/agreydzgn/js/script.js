c56f247ebdfd4ee5f3b29f476f4e2aec2981fd80
Agrey Tosira <agreytosira@gmail.com>
2020-06-07 21:27:11 +0700

    Update AgreyDZGN

--- p/agreydzgn/js/script.js
+++ p/agreydzgn/js/script.js
@@ -4,3 +4,27 @@ $(document).ready(function () {
         duration: 1000,
     });
 });
+
+const navbar = document.querySelector(".navbar");
+const sectionOne = document.querySelector(".jumbotron");
+
+const sectionOneOptions = {};
+
+const sectionOneObserver = new IntersectionObserver(function (
+        entries,
+        sectionOneObserver
+    ) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                navbar.classList.add('transparent');
+                navbar.classList.remove('shadow');
+            } else {
+                navbar.classList.remove('transparent');
+                navbar.classList.add('shadow');
+            }
+        });
+    },
+    sectionOneOptions);
+
+
+sectionOneObserver.observe(sectionOne);
