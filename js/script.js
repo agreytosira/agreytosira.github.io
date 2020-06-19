a1d3139e4e443453291faab247ab2840e04c9f25
Agrey Tosira <agreytosira@gmail.com>
2020-06-19 16:09:47 +0700

    Ganti Layout

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
-// All animations will take exactly 500ms
-var scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 500,
-    speedAsDuration: true
-});
+// AnimateOnScroll.js
+AOS.init();
+
+// Hide on Scroll Navbar
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+
+    var currentScrollpos = window.pageYOffset;
+    if (prevScrollpos > currentScrollpos) {
+        document.getElementById("navbar").style.top = "0";
+        document.getElementById("navbar").style.background = "#222222";
+    } else {
+        document.getElementById("navbar").style.top = "-300px";
+    }
+
+    prevScrollpos = currentScrollpos;
+
+};
+
+// Typed.js
 
 $('document').ready(function () {
     var typed = new Typed('#typed', {
         strings: ["Front-end Dev", "Graphic Designer", "Freelancer"],
-        backSpeed: 40,
+        backSpeed: 10,
         typeSpeed: 40,
         loop: true
     });
