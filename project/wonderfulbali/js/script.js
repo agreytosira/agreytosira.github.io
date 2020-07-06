5888732407549e7e1a0d04b7aa6e2ad7bf9aa4ac
agreytosira <agreytosira@gmail.com>
2020-07-06 20:03:04 +0700

    Optimasi Gambar Wonderful Bali

--- project/wonderfulbali/js/script.js
+++ project/wonderfulbali/js/script.js
@@ -1,1 +1,18 @@
 AOS.init();
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+
+  var currentScrollpos = window.pageYOffset;
+  if (prevScrollpos > currentScrollpos) {
+    document.getElementById("contact-nav").style.top = "0";
+    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.background = "#222222";
+  } else {
+    document.getElementById("contact-nav").style.top = "-300px";
+    document.getElementById("navbar").style.top = "-40px";
+  }
+
+  prevScrollpos = currentScrollpos;
+
+};
