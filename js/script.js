289bd8518964f1df1fac0d8717391c9769042222
agreytosira <agreytosira@gmail.com>
2020-07-24 06:39:31 +0700

    Update Portfolio + All Fix

--- js/script.js
+++ js/script.js
@@ -25,98 +25,12 @@ $('document').ready(function () {
     // Typed.js
 
     var typed = new Typed('#typed', {
-        strings: ["Front-end Dev", "Graphic Designer", "UI/UX Designer", "Freelancer"],
+        strings: ["Front-end Dev", "UI/UX Designer", "Freelancer"],
         backSpeed: 10,
         typeSpeed: 40,
         loop: true
     });
 });
-
-
-
-$('document').ready(function () {
-    // Project Button
-
-    $('.button-all').click(function () {
-        $('.webdesign').show('slow');
-        $('.mobileappdesign').show('slow');
-        $('.posterdesign').show('slow');
-        $('.logodesign').show('slow');
-        $('.fbcoverdesign').show('slow');
-        $('.tshirtdesign').show('slow');
-        $('.pricelistdesign').show('slow');
-    })
-
-    $('.button-webdesign').click(function () {
-        $('.webdesign').show('slow');
-        $('.mobileappdesign').hide('slow');
-        $('.posterdesign').hide('slow');
-        $('.logodesign').hide('slow');
-        $('.fbcoverdesign').hide('slow');
-        $('.tshirtdesign').hide('slow');
-        $('.pricelistdesign').hide('slow');
-    });
-
-    $('.button-mobileappdesign').click(function () {
-        $('.webdesign').hide('slow');
-        $('.mobileappdesign').show('slow');
-        $('.posterdesign').hide('slow');
-        $('.logodesign').hide('slow');
-        $('.fbcoverdesign').hide('slow');
-        $('.tshirtdesign').hide('slow');
-        $('.pricelistdesign').hide('slow');
-    });
-
-    $('.button-posterdesign').click(function () {
-        $('.webdesign').hide('slow');
-        $('.mobileappdesign').hide('slow');
-        $('.posterdesign').show('slow');
-        $('.logodesign').hide('slow');
-        $('.fbcoverdesign').hide('slow');
-        $('.tshirtdesign').hide('slow');
-        $('.pricelistdesign').hide('slow');
-    });
-
-    $('.button-pricelistdesign').click(function () {
-        $('.webdesign').hide('slow');
-        $('.mobileappdesign').hide('slow');
-        $('.posterdesign').hide('slow');
-        $('.logodesign').hide('slow');
-        $('.fbcoverdesign').hide('slow');
-        $('.tshirtdesign').hide('slow');
-        $('.pricelistdesign').show('slow');
-    });
-
-    $('.button-logodesign').click(function () {
-        $('.webdesign').hide('slow');
-        $('.mobileappdesign').hide('slow');
-        $('.posterdesign').hide('slow');
-        $('.logodesign').show('slow');
-        $('.fbcoverdesign').hide('slow');
-        $('.tshirtdesign').hide('slow');
-        $('.pricelistdesign').hide('slow');
-    });
-
-    $('.button-fbcoverdesign').click(function () {
-        $('.webdesign').hide('slow');
-        $('.mobileappdesign').hide('slow');
-        $('.posterdesign').hide('slow');
-        $('.logodesign').hide('slow');
-        $('.fbcoverdesign').show('slow');
-        $('.tshirtdesign').hide('slow');
-        $('.pricelistdesign').hide('slow');
-    });
-
-    $('.button-tshirtdesign').click(function () {
-        $('.webdesign').hide('slow');
-        $('.mobileappdesign').hide('slow');
-        $('.posterdesign').hide('slow');
-        $('.logodesign').hide('slow');
-        $('.fbcoverdesign').hide('slow');
-        $('.tshirtdesign').show('slow');
-        $('.pricelistdesign').hide('slow');
-    });
-})
 
 //Script Redirect CTRL + U
 function redirectCU(e) {
