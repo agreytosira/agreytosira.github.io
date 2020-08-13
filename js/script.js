6a83dd1933b24e187ad4c50e3cc66ebf6f5bd24b
agreytosira <agreytosira@gmail.com>
2020-08-13 23:02:26 +0700

    Update Versi 2.0

--- js/script.js
+++ js/script.js
@@ -1,15 +1,8 @@
 $('document').ready(function () {
-    setTimeout(function () {
-        $('.loader.bg').fadeToggle();
-    }, 1500);
-
-    // Typed.js
     var typed = new Typed('#typed', {
-        strings: ["Front-end Developer", "UI/UX Designer", "Graphic Designer"],
+        strings: ["Front-end Web Developer", "Front-end Web Developer", "Front-end Web Developer"],
         backSpeed: 10,
         typeSpeed: 40,
         loop: true
     });
-
-
 });
