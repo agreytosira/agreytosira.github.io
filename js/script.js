072845c83bc649c44ba9632b21d0c544e3aaf1a7
Agrey Tosira <agreytosira@gmail.com>
2020-12-14 20:04:10 +0700

    Update to New Version Portfolio

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// Navbar Animate on Scroll
-window.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".navbar");
-    navbar.classList.toggle("scrolled", window.scrollY > 0)
-    navbar.classList.toggle("navbar-dark", window.scrollY > 0)
-});
